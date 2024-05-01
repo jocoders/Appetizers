e49dfc28f6eac1a466ee78e8627ac5771c2f3da7
Evgenii Kireev <jocodersapp@gmail.com>
2024-05-01 07:59:49 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Evgeniy Kireev on 1/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
